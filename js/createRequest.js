2a6f593644b260d69e04b570b9b714423437ad5f
Tepokun <artememelyanow@gmail.com>
2023-09-22 08:15:25 +0300

    Update createRequest.js

--- js/createRequest.js
+++ js/createRequest.js
@@ -1,27 +1,69 @@
-function createRequest(method, url, data, callback) {
-    const xhr = new XMLHttpRequest();
-  
-    xhr.open(method, url);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.responseType = 'json';
-  
-    xhr.onerror = () => {
-      console.error('Ошибка при отправке запроса');
-      callback(null);
+function createRequest(requestBodyString, requestSourceString = "", callback, uploadInfoIsNeed = false) {
+
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://jscp-diplom.netoserver.ru/");
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.send(requestBodyString);
+
+  if (uploadInfoIsNeed) {
+    xhr.upload.onprogress = function (event) {
+      console.log(`Отправка данных... Отправлено ${event.loaded} из ${event.total} байт`);
     };
-  
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status >= 200 && xhr.status < 300) {
-        try {
-          const response = xhr.response;
-          callback(response);
-        } catch (error) {
-          console.error('Ошибка обработки ответа:', error);
-          callback(null);
-        }
-      }
+
+    xhr.upload.onerror = function () {
+      console.log("Произошла ошибка при загрузке данных на сервер!");
     };
-  
-    xhr.send(data);
   }
-  
+
+  xhr.onload = function () {
+    if (xhr.status != 200) {
+      alert("Ошибка: " + xhr.status);
+      return;
+    }
+
+    console.log(`${requestSourceString} - статус запроса: ${xhr.status} (${xhr.statusText})`);
+    callback(xhr.response);
+
+  };
+
+  xhr.onerror = function () {
+    alert("Запрос не удался");
+  };
+
+};
+
+function setItem(key, value) {
+  try {
+    return window.sessionStorage.setItem(key, value);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function getItem(key, value) {
+  try {
+    return window.sessionStorage.getItem(key);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function setJSON(key, value) {
+  try {
+    const json = JSON.stringify(value);
+
+    setItem(key, json);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+function getJSON(key) {
+  try {
+    const json = getItem(key);
+
+    return JSON.parse(json);
+  } catch (e) {
+    console.error(e);
+  }
+}
