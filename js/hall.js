f4a74f68232134b36feb89e0cac6bc7a9b57a9f9
Tepokun <artememelyanow@gmail.com>
2023-09-22 08:15:23 +0300

    Update hall.js

--- js/hall.js
+++ js/hall.js
@@ -1,72 +1,176 @@
-const seanceData = localStorage.getItem('seance-data');
-const parsedSeances = JSON.parse(seanceData);
+document.addEventListener("DOMContentLoaded", () => {
 
-const confStepWrapper = document.querySelector('.conf-step__wrapper');
-const movieTitle = document.querySelector('.buying__info-title');
-movieTitle.innerText = parsedSeances.filmName;
+  const dataOfTheSelectedSeance = getJSON("data-of-the-selected-seance");
 
-const movieSeanceStart = document.querySelector('.buying__info-start');
-movieSeanceStart.innerText = `Начало сеанса:  ${parsedSeances.seanceTime}`;
+  const timestamp = +dataOfTheSelectedSeance.seanceTimeStamp / 1000;
+  const hallId = dataOfTheSelectedSeance.hallId;
+  const seanceId = dataOfTheSelectedSeance.seanceId;
+  const requestBodyString = `event=get_hallConfig&timestamp=${timestamp}&hallId=${hallId}&seanceId=${seanceId}`;
 
-const hallName = document.querySelector('.buying__info-hall'); 
-hallName.innerText = parsedSeances.hallName;
+  createRequest(requestBodyString, "HALL", updateHtmlHall);
+});
 
-const acceptinButton = document.querySelector('.acceptin-button');
-const priceStandart = document.querySelector('.price-standart');
-priceStandart.innerText = parsedSeances.hallPriceStandart;
+function updateHtmlHall(serverResponse) {
+  const response = JSON.parse(serverResponse);
+  const dataOfTheSelectedSeance = getJSON("data-of-the-selected-seance");
 
-const priceVip = document.querySelector('.price-vip');
-priceVip.innerText = parsedSeances.hallPriceVip;
+  let configSelectedHall;
+  let configHalls = getJSON("config-halls");
+  if (response !== null) {
+    console.info("Есть данные в ответе сервера по залу...");
+    configSelectedHall = response;
+  } else {
+    console.info("В зале нет купленных мест...");
+    configSelectedHall = configHalls[dataOfTheSelectedSeance.hallId];
+  }
 
-createRequest('POST', 'https://jscp-diplom.netoserver.ru/', `event=get_hallConfig&timestamp=${parsedSeances.seanceTimeStamp}&hallId=${parsedSeances.hallId}&seanceId=${parsedSeances.seanceId}`, function (response) {
-    if(response) {
-       parsedSeances.hallConfig = response;     
-    } else {
-        console.log('Нет купленных билетов');
+  const buyingInfoSection = document.querySelector(".buying__info");
+  buyingInfoSection.innerHTML = "";
+
+  const textHtml = `
+  <div class="buying__info-description">
+    <h2 class="buying__info-title">"${dataOfTheSelectedSeance.filmName}"</h2>
+    <p class="buying__info-start">Начало сеанса: ${dataOfTheSelectedSeance.seanceTime
+    } </br>
+    ${new Date(+dataOfTheSelectedSeance.seanceTimeStamp).toLocaleDateString(
+      "ru-RU",
+      { day: "2-digit", month: "long", year: "numeric" }
+    )}</p>
+    <p class="buying__info-hall">${dataOfTheSelectedSeance.hallName
+    }</p>          
+  </div>
+  <div class="buying__info-hint">
+    <p>Тапните дважды,<br>чтобы увеличить</p>
+  </div>
+`;
+  buyingInfoSection.insertAdjacentHTML("beforeend", textHtml);
+
+  const confStep = document.querySelector(".conf-step");
+  const textHtmlConf = `
+  <div class="conf-step__wrapper">
+  ${configSelectedHall}
+  </div>
+`;
+
+  confStep.innerHTML = "";
+  confStep.insertAdjacentHTML("beforeend", textHtmlConf);
+
+  const textHtmlLegend = `
+    <div class="conf-step__legend">
+    <div class="col">
+      <p class="conf-step__legend-price"><span class="conf-step__chair conf-step__chair_standart"></span> Свободно (<span
+          class="conf-step__legend-value price-standart">${dataOfTheSelectedSeance.priceStandart}</span>руб)</p>
+      <p class="conf-step__legend-price"><span class="conf-step__chair conf-step__chair_vip"></span> Свободно VIP (<span
+          class="conf-step__legend-value price-vip">${dataOfTheSelectedSeance.priceVip}</span>руб)</p>
+    </div>
+    <div class="col">
+      <p class="conf-step__legend-price"><span class="conf-step__chair conf-step__chair_taken"></span> Занято</p>
+      <p class="conf-step__legend-price"><span class="conf-step__chair conf-step__chair_selected"></span> Выбрано</p>
+    </div>
+  </div>
+`;
+  confStep.insertAdjacentHTML("beforeend", textHtmlLegend);
+
+  const selectedChairs = [];
+
+  const confStepChair = document.querySelectorAll(
+    ".conf-step__wrapper .conf-step__chair"
+  );
+
+  confStepChair.forEach((element) => {
+    element.addEventListener("click", (event) => {
+      const elementClickClassList = event.currentTarget.classList;
+      if (
+        elementClickClassList.contains("conf-step__chair_disabled") ||
+        elementClickClassList.contains("conf-step__chair_taken")
+      ) {
+        return;
+      }
+      element.classList.toggle("conf-step__chair_selected");
+    });
+  });
+
+  const acceptinButton = document.querySelector(".acceptin-button");
+
+  acceptinButton?.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const arrayOfRows = Array.from(
+      document.querySelectorAll(".conf-step__row")
+    );
+
+    for (let indexRow = 0; indexRow < arrayOfRows.length; indexRow++) {
+      const elementRow = arrayOfRows[indexRow];
+      const arrayOfChairs = Array.from(
+        elementRow.querySelectorAll(".conf-step__chair")
+      );
+
+      for (
+        let indexChair = 0;
+        indexChair < arrayOfChairs.length;
+        indexChair++
+      ) {
+        const elementChair = arrayOfChairs[indexChair];
+        if (elementChair.classList.contains("conf-step__chair_selected")) {
+          const typeChair = elementChair.classList.contains(
+            "conf-step__chair_vip"
+          )
+            ? "vip"
+            : "standart";
+
+          selectedChairs.push({
+            row: indexRow + 1,
+            place: indexChair + 1,
+            typeChair: typeChair,
+          });
+        }
+      }
     }
 
-    confStepWrapper.innerHTML = parsedSeances.hallConfig;
-    
-    const updateAcceptinButtonState = () => {
-        const arrSelectedChairs = document.querySelectorAll('.conf-step__row .conf-step__chair_selected');
-        if(arrSelectedChairs.length > 0) {
-            acceptinButton.removeAttribute('disabled');
-        } else {
-            acceptinButton.setAttribute('disabled', 'disabled');
-        }
-    };
-    
-    document.querySelectorAll('.conf-step__chair').forEach((chair) => {
-        chair.addEventListener('click', (event) => {
-            if(event.target.classList.contains('conf-step__chair_taken')) {
-                return;
-            }
-            event.target.classList.toggle('conf-step__chair_selected');
-            updateAcceptinButtonState();
-        });
-    });
+    if (selectedChairs.length) {
+      setJSON("data-of-the-selected-chairs", selectedChairs);
 
-    updateAcceptinButtonState();
+      const configSelectedHallHtml = document
+        .querySelector(".conf-step__wrapper")
+        ?.innerHTML.trim();
 
-    acceptinButton.addEventListener('click', () => {
-        const selectedChairs = [...document.querySelectorAll('.conf-step__row .conf-step__chair_selected')].map((selectedChair) => {
-            const rowElement = selectedChair.closest('.conf-step__row');   
-            const rowIndex = Array.from(rowElement.parentNode.children).indexOf(rowElement) + 1;
-            const placeIndex = Array.from(rowElement.children).indexOf(selectedChair) + 1;
-            
-            let typePlace;
-            if (selectedChair.classList.contains('conf-step__chair_standart')){
-                typePlace = 'standart';
-            } else if (selectedChair.classList.contains('conf-step__chair_vip')){
-                typePlace = 'vip';
-            }
+      configHalls[dataOfTheSelectedSeance.hallId] = configSelectedHallHtml;
+      setJSON("config-halls", configHalls);
 
-            return { row: rowIndex, place: placeIndex, type: typePlace };
-        });
+      confStepChair.forEach((element) => {
+        element.classList.replace("conf-step__chair_selected", "conf-step__chair_taken");
+      });
 
-        parsedSeances.hallConfig = confStepWrapper.innerHTML;
-        parsedSeances.selectedPlaces = selectedChairs;
-        localStorage.setItem('seance-data', JSON.stringify(parsedSeances));
-        window.location.href = "payment.html";
-    });    
-});
+      const configSelectedHallTaken = document.querySelector(".conf-step__wrapper")?.innerHTML.trim();
+      const configHallsTaken = getJSON("config-halls");
+
+      configHallsTaken[dataOfTheSelectedSeance.hallId] = configSelectedHallTaken;
+      setJSON("pre-config-halls-paid-seats", configHallsTaken);
+
+      const dataOfTheSelectedChairs = getJSON("data-of-the-selected-chairs");
+
+      const arrRowPlace = [];
+      let totalCost = 0;
+
+      dataOfTheSelectedChairs.forEach(element => {
+        arrRowPlace.push(`${element.row}/${element.place}`);
+        totalCost += element.typeChair === "vip" ? +dataOfTheSelectedSeance.priceVip : +dataOfTheSelectedSeance.priceStandart;
+      });
+
+      const strRowPlace = arrRowPlace.join(", ");
+
+      const ticketDetails = {
+        ...dataOfTheSelectedSeance,
+        strRowPlace: strRowPlace,
+        hallNameNumber: dataOfTheSelectedSeance.hallName.slice(3).trim(),
+        seanceTimeStampInSec: +dataOfTheSelectedSeance.seanceTimeStamp / 1000,
+        seanceDay: new Date(+dataOfTheSelectedSeance.seanceTimeStamp).toLocaleDateString("ru-RU", { day: "2-digit", month: "short", year: "numeric" }),
+        totalCost: totalCost,
+      };
+
+      setJSON("ticket-details", ticketDetails);
+
+      window.location.href = "payment.html";
+    }
+  });
+};
